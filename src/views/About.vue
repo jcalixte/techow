8cc466c891c4234b49680d99a35adc24d05a31d0
Julien Calixte <julienc@bam.tech>
2020-03-24 20:36:16 +0100

    :bento: (logo) add logo to app

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      Icons made by
+      <a href="https://www.flaticon.com/authors/icongeek26" title="Icongeek26"
+        >Icongeek26</a
+      >
+      from
+      <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+    </div>
   </div>
 </template>
